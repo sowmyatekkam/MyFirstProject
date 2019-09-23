d15b159c6f6ad4cf3ee6f79784c5f6ce81a4a267
sowmyatekkam <55706776+sowmyatekkam@users.noreply.github.com>
2019-09-23 11:54:17 -0400

    this is my first commit
    sdfsdf

---
+++ BankRepository.java
@@ -0,0 +1,10 @@
+package com.uiexample.BankProjectwithWebService;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface BankRepository extends CrudRepository<BankCustomerSowmya, String>{
+
+}
